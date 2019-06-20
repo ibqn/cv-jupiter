54f931c7788366b17b5cf8254aa047654275da3a
Evgeny Bobkin <evgen.ibqn@gmail.com>
2019-06-20 11:28:10 +0200

    update dependencies

--- jupyter.bash
+++ jupyter.bash
@@ -7,6 +7,8 @@ python3 -m venv './venv'
 source './venv/bin/activate'
 
 python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade setuptools
+
 python3 -m pip install --upgrade --requirement './requirements.txt'
 
 exec jupyter notebook --no-browser --port 9999
